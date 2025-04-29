bbb68a1a71ceda2c4638eba5a21a52a982d05906
Syed Mahmoud Sobhani <84802517+Danger-XD@users.noreply.github.com>
2025-04-29 18:46:21 +0600

    version 2.0

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".downCv-btn").addEventListener("click", function () {
+    const cvPath = "assets/Syed Mahmoud Sobhani_main_CV_.pdf"; // Change the path to your CV file
+    const link = document.createElement("a");
+    link.href = cvPath;
+    link.download = "Syed Mahmoud Sobhani_main_CV.pdf"; // Name of the downloaded file
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
+});
