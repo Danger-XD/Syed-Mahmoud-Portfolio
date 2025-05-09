3a5c5db7602b4bde3ac949d508d7870efaec2f83
Syed Mahmoud Sobhani <84802517+Danger-XD@users.noreply.github.com>
2025-05-09 19:43:14 +0600

    cv updated

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".downCv-btn").addEventListener("click", function () {
-    const cvPath = "assets/Syed Mahmoud Sobhani_main_CV_.pdf"; // Change the path to your CV file
+    const cvPath = "assets/Syed Mahmoud Sobhani___CV.pdf"; // Change the path to your CV file
     const link = document.createElement("a");
     link.href = cvPath;
-    link.download = "Syed Mahmoud Sobhani_main_CV.pdf"; // Name of the downloaded file
+    link.download = "Syed Mahmoud Sobhani_CV.pdf"; // Name of the downloaded file
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
